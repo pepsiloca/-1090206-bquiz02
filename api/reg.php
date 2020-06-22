c72072c888560ca185ede871cea155f2e2aef843
u8931011 <u8931011@gmail.com>
2020-06-22 16:22:26 +0800

    完成會員註冊功能＂

---
+++ api/reg.php
@@ -0,0 +1,11 @@
+<?php 
+
+include_once "../base.php";
+$db=new DB("user");
+/* $acc=$_POST['acc'];
+$pw=$_POST['pw'];
+$email=$_POST['email']; */
+
+echo $db->save($_POST);  //快速寫法
+
+?>
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-﻿﻿<?php include_once "base.php";?>
+﻿﻿﻿<?php include_once "base.php";?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0039) -->
 <html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -76,7 +76,7 @@
             </div>
         </div>
         <div id="bottom">
-    	    本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2012健康促進網社群平台 All Right Reserved 
+    	    本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2020健康促進網社群平台 All Right Reserved 
     		 <br>
     		 服務信箱：health@test.labor.gov.tw<img src="./icon/02B02.jpg" width="45">
         </div>

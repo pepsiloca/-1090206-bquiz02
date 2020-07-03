f73e631f33032e03862e6575e1aed415cf8349f3
u8931011 <u8931011@gmail.com>
2020-07-03 16:01:15 +0800

    完成人氣文章功能

--- api/good.php
+++ api/good.php
@@ -7,14 +7,17 @@ $Log=new DB("log");
 $id=$_POST['id'];
 $type=$_POST['type'];
 $user=$_POST['user'];
-
+$post=$News->find($id);
 switch($type){
     case 1:
         $Log->save(['news'=>$id,'user'=>$user]);
+        $post['good']++;
+        $News->save($post);
     break;
     case 2:
         $Log->del(['news'=>$id,'user'=>$user]);
-
+        $post['good']--;
+        $News->save($post);
     break;
     
 }
--- front/pop.php
+++ front/pop.php
@@ -1,36 +1,71 @@
+<style>
+    .all {
+        background: rgba(51, 51, 51, 0.8);
+        color: #FFF;
+        min-height: 100px;
+        width: 300px;      
+        position: fixed;
+        display: none;
+        z-index: 9999;
+        overflow: auto;
+    }
+
+    .title {
+        background: #eee;
+        cursor: pointer;
+    }
+</style>
 <fieldset>
     <legend>目前位置：首頁 > 人氣文章區</legend>
     <table>
-    <tr>
-        <td width="20%">標題</td>
-        <td width="60%">內容</td>
-        <td width="20%">人氣</td>
-    </tr>
+        <tr>
+            <td width="20%">標題</td>
+            <td width="60%">內容</td>
+            <td width="20%">人氣</td>
+        </tr>
 
-<?php
+        <?php
     $db=new DB("news");
     $total=$db->count();
     $div=5;
     $pages=ceil($total/$div);
     $now=(!empty($_GET['p']))?$_GET['p']:1;
     $start=($now-1)*$div;
-    $rows=$db->all([],"order by good desc limit $start,$div");
-
+    $rows=$db->all([]," order by good desc limit $start,$div");
+    $log=new DB('log');
     foreach($rows as $row){
 ?>
-    <tr>
-        <td><?=$row['title'];?></td>
-        <td><?=mb_substr($row['text'],0,20,'utf8');?> ...</td>
-        <td></td>
-    </tr>
-<?php
+        <tr>
+            <td class="title"><?=$row['title'];?></td>
+            <td class="tt">
+                <div class="abbr"><?=mb_substr($row['text'],0,20,'utf8');?> ...</div>
+                <div class="all"><?=nl2br($row['text']);?></div>
+            </td>
+            <td>
+
+                <span id="vie<?=$row['id'];?>"><?=$row['good'];?></span>個人說<img src='icon/02B03.jpg' style="width:20px">
+
+                <?php
+        if(!empty($_SESSION['login'])){
+            $chk=$log->count(['user'=>$_SESSION['login'],'news'=>$row['id']]);
+            if($chk>0){
+                echo "<a href='#' id='good".$row['id']."' onclick='good(".$row['id'].",2,&#39;".$_SESSION['login']."&#39;)'>收回讚</a>";
+            }else{
+                echo "<a href='#' id='good".$row['id']."' onclick='good(".$row['id'].",1,&#39;".$_SESSION['login']."&#39;)'>讚</a>";
+            }
+        }
+
+        ?>
+            </td>
+        </tr>
+        <?php
     }
 ?>
-</table>
-<div>
-<?php
+    </table>
+    <div>
+        <?php
 
-//分頁功能的設定
+        //分頁功能設定
 if(($now-1)>0){
     echo "<a href='?do=pop&p=".($now-1)."' > < </a>";
 }
@@ -45,5 +80,16 @@ if(($now+1)<=$pages){
 }
 
 ?>
-</div>
+    </div>
 </fieldset>
+
+<script>
+    $(".title").hover(function(){
+        $(this).next().children('.all').toggle();
+    })
+    $(".tt").hover(function(){
+        $(this).children('.all').toggle();
+    })
+
+
+</script>

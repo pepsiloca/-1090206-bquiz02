c2f92eb327d6daa7dfaefc0808c3395ec88f2847
u8931011 <u8931011@gmail.com>
2020-07-03 11:10:41 +0800

    完成分類網誌文章內容顯示功能

--- front/po.php
+++ front/po.php
@@ -1,36 +1,47 @@
 <style>
-
     fieldset{
         display:inline-block;
         vertical-align: top;
-        margin-top:20px;
-
+        margin-top:20px ;
     }
-
-.item{
-    display:block;
-    margin:5px 10px;
-}
-
+    .item,
+    .list-item{
+        display: block;
+        margin:5px 10px;
+    }
 </style>
-<div>目前位置: 首頁 > 分類網誌 > <span id="nav">5555</span></div>
+<div>目前位置：首頁 > 分類網誌 > <span id="nav"></span></div>
 <fieldset>
     <legend>分類網誌</legend>
-<a class='item' href="javascript:showlist(1)"> 健康新知</a>
-<a class='item' href="javascript:showlist(2)"> 菸害防治</a>
-<a class='item' href="javascript:showlist(3)"> 癌症防治</a>
-<a class='item' href="javascript:showlist(4)"> 慢性病防治</a>
+<a class='item' href="javascript:showList(1)">健康新知</a>
+<a class='item' href="javascript:showList(2)">菸害防治</a>
+<a class='item' href="javascript:showList(3)">癌症防治</a>
+<a class='item' href="javascript:showList(4)">慢性病防治</a>
 </fieldset>
-
 <fieldset style="width:75%">
     <legend>文章列表</legend>
-    </fieldset>
+    <div class="list"></div>
+    <div class="text"></div>
+</fieldset>
 
-    <script>
-        showlist(1)   //先看到健康新知的內容
 
-        function showlist(type){
-            let str=["健康新知","菸害防制","癌症防治","慢性病防治"]
-            $("#nav").html(str[type-1])
-        }
+<script>
+showList(1)
+function showList(type){
+    let str=["健康新知","菸害防治","癌症防治","慢性病防治"]
+    $("#nav").html(str[type-1])
+    $.get("api/get_list.php",{type},function(list){
+        $(".list").html(list)
+        $(".text").hide()
+        $(".list").show()
+    })
+}
+function showPost(id){
+    $.get("api/get_post.php",{id},function(post){
+        $(".text").html(post)
+        $(".list").hide();
+        $(".text").show();
+    })
+}
+</script>
     </script>

4cefb8819fbb27372445d5c3d137bffec49c3e9c
u8931011 <u8931011@gmail.com>
2020-07-06 16:19:14 +0800

    前端問卷列表

---
+++ front/vote.php
@@ -0,0 +1,27 @@
+
+<?php
+    $q=$_GET['q'];
+    $db=new DB('que');
+    $subject=$db->find($q);
+    $options=$db->all(['parent'=>$q]);
+
+?>
+
+<fieldset>
+    <legend>目前位置：首頁 > 問卷調查 > <?=$subject['text'];?></legend>
+    <h3><?=$subject['text'];?></h3>
+    <form action="api/vote.php" method="post">
+        <table>
+        <?php
+            foreach($options as $key => $row){
+        ?>
+            <tr>
+                <td><input type="radio" name="vote" value="<?=$row['id'];?>"><?=$row['text'];?></td>
+           </tr>
+            <?php
+            }
+            ?>
+        </table>
+    <div class="ct"><input type="submit" value="我要投票"></div>
+    </form>
+</fieldset>
